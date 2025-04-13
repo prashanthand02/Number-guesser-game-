1b348e03e7fa06108fb8af8a3ee9531fbdb93ae9
prashanthand02 <prashanthanand873@gmail.com>
2025-04-13 12:53:57 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,13 @@
 # Number-guesser-game-
+import random
 print("Welcome there")
+top_range = input("Enter the range u want to play in: ")
+print("You get 5 attempts")
+if top_range.lstrip("-").isnumeric():
+  top_range = int(top_range)
+  if top_range <= 0:
+    print("Range should be higher than zero")
+  else:
+    x = top_range
+else:
+  print("Enter a number")
